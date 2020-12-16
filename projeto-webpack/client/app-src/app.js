dc524fec6ea2adde14fd4c26af34171681d7312d
yano1996 <marcosyano@live.com.pt>
2020-12-16 19:15:45 -0300

    Curso de Webpack finalizado

---
+++ projeto-webpack/client/app-src/app.js
@@ -0,0 +1,26 @@
+import { NegociacaoController } from './controllers/NegociacaoController.js';
+import { Negociacao } from './domain/index.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'jquery/dist/jquery.js';
+import 'bootstrap/js/modal.js';
+import '../css/meuacesso.css';
+
+$('h1').click(()=> alert('voce me clicou'));
+console.log($('h1').modal);
+
+const controller = new NegociacaoController();
+const negociacao = new Negociacao(new Date(), 1, 200);
+const headers = new Headers();
+headers.set('Content-Type', 'application/json');
+const body = JSON.stringify(negociacao);
+const method = 'POST';
+
+const config = {
+    method,
+    headers,
+    body
+};
+
+fetch(`${SERVICE_URL}/negociacoes `, config)
+    .then(() => console.log('Dado enviado com sucesso'));
